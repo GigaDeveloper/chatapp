6978132384e1e0fa94df51e1f300e4f99e6e45ac
Giga Songulashvili <giga@unipay.ge>
2017-03-22 18:48:49 +0400

    Update v1

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 
 const port = process.env.PORT || 8888;
 
